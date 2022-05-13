cab2d5507dcf12970041d292d934b0dde386ce16
Franciley Rodrigues <testepa45@gmail.com>
2022-05-13 16:21:25 -0300

    dia 2  bloco4

---
+++ trybe-exercises/fundamentos/bloco4-dia1-javaScript/dia-2-script.js
@@ -0,0 +1,90 @@
+/*  EXERCICIO 1 tasksList.unshift puxa para primeira posição
+ */
+
+
+/* let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push("coco babaçu")
+
+console.log(menu); */
+
+
+/* let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+tasksList.unshift('Fazer exercícios da Trybe');
+console.log(tasksList); */
+
+/* 
+EXERCICIO 2 REMOVER ELEMENTO DO ARRY */
+  
+
+
+/* 
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+ */
+/* tasksList.pop([1]);  // remove a última tarefa
+console.log(tasksList); */
+
+/* tasksList.shift();  // remove a última tarefa
+console.log(tasksList) */
+
+
+   
+/* EXERCICIO 3 ENCONTRA ELEMENTO DENTRO DO ARRY ARRY */
+
+
+/* let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+let indexOfTask = tasksList.indexOf('Reunião');
+console.log(indexOfTask); */
+
+
+
+/* let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf("Serviços");
+
+console.log(menuServices); */
+
+/* let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf("portfólio") ;
+
+console.log(indexOfPortfolio); */
+
+/* EXERCICIO 4 for multiplicando numero */
+
+/* let numero = 5;
+for(var index = 1; index <=9; index ++) {
+    console.log(numero * index)
+    console.log("*")
+} */
+
+
+/* EXERCICIO 5 for imprimindo lista de array */
+
+/* 
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (var index = 0; index < groceryList.length; index += 1 ) {
+    console.log(groceryList[index])
+} */
+ 
+
+/* EXERCICIO 6 for/of deixa o for mas curto é simplis */
+
+
+
+/* let numeros = [1,2,3,4,5];
+for(let numero of numeros) {
+  console.log(numero);
+}
+
+// resultado:
+//1
+//2
+//3
+//4
+//5 */
+
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let nomes of names) {
+    console.log(nomes)
+}
