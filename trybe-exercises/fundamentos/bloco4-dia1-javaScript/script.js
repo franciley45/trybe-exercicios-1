c45a0e74c9c8e030f88bd0bd9f60fa8e7997ae40
Franciley Rodrigues <testepa45@gmail.com>
2022-05-13 12:28:29 -0300

    dia 1 de javascript bloco04

---
+++ trybe-exercises/fundamentos/bloco4-dia1-javaScript/script.js
@@ -0,0 +1,117 @@
+/* let trafficLight = "rocho";
+
+switch (trafficLight) {
+    case "vermelhor":
+        console.log("pare");
+        break;
+
+        case "amarelo":
+            console.log("atenção");
+            break;
+
+            case "verde":
+            console.log("siga");
+            break;
+
+            default:
+        console.log("nunhuma das condicoes");
+}
+ */
+
+ /* exercio 2 abaixo */
+
+/* let candidata = "aprovada";
+switch (candidata) {
+     case "reprovado":
+         console.log("não foi dessa vez");
+         break;
+         
+         case "lista":
+             console.log("espere mas um pouco");
+             break;
+
+             case "aprovada":
+                 console.log("parabens vc foi aprovado");
+                 break;
+
+            default:
+                console.log("nenhuma das opcoẽs");
+
+} */
+
+ /* exercio 3 abaixo */
+
+
+/* let a = 10;
+let b = 5; */
+ 
+/* let soma = a + b;
+console.log(soma) */
+
+/* let subtracao = a - b;
+console.log(subtracao); */
+
+/* let multiplicacao = a * b;
+console.log(multiplicacao); */
+
+/* let divisao = a / b;
+console.log(divisao); */
+
+/* let modulo = a % b;
+console.log(modulo); */
+
+ /* exercio 4 abaixo */
+
+/* let valor1 = 89;
+let valor2 = 23;
+if (valor1 > valor2) {
+    console.log(valor1);
+} else if (valor2 > valor1) {
+    console.log(valor2);
+} */
+
+ /* exercio 5 abaixo */
+
+/*  let valor1 = 89;
+ let valor2 = 23;
+ let valor3 = 120;
+ 
+ if (valor1 > valor2 && valor1 > valor3) {
+    console.log(valor1);
+ } else if (valor2 > valor1 && valor2 > valor3) {
+     console.log(valor2);
+ } else {
+     console.log(valor3);
+ } */
+
+
+ /* exercio 6 abaixo */
+
+
+/*  const definido = 0;
+if (definido > 0) {
+    console.log("positivo");
+} else if (definido < 0) {
+    console.log("negativo");
+} else {
+    console.log("zero")
+} */
+
+
+/* exercio 7 abaixo */
+
+
+/* let angolo1 = 60;
+let angolo2 = 100;
+let angolo3 = 20;
+
+if ( 180 === angolo1 + angolo2 + angolo3 ) {
+    console.log(true)
+} else if ( 180 !== angolo1 + angolo2 + angolo3 ) {
+    console.log(false)
+} else {
+    console.log("erro")
+}
+ */
+
+
